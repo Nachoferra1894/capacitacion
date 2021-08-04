a278057fb28e0cfa7c802ff0f02633349cffa7ec
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-08-03 22:32:13 -0300

    first changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Background from '../img/Back.jpeg'
 
 const Home = () => {
     return (
-        <div >
+        <div>
             <div className="home-div-back">
                 <img width="100%" src={Background}></img>
             </div>
