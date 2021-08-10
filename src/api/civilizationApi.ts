455053539bdcfa72b1f012a9770dfb7bdb1cabd6
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-08-10 12:16:35 -0300

    changed to https

--- src/api/civilizationApi.ts
+++ src/api/civilizationApi.ts
@@ -2,7 +2,7 @@ import { get, post } from '../utils/httpUtils';
 
 class CivilizationApi {
     getCivilizations() {
-        return get('http://localhost:8080/hello')
+        return get('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
     }
 }
  
